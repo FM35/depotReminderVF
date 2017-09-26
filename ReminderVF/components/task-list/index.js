10ce6654f754b702b450adcddca9f178a58ef30e
François Massiot <francois.massiot@outlook.fr>
2017-09-26 15:46:22 +0200

    V0.3 List and  status

--- ReminderVF/components/task-list/index.js
+++ ReminderVF/components/task-list/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import { List, ListItem } from 'react-native-elements';
+import { List, ListItem, Badge } from 'react-native-elements';
+import { TASK } from '../../model';
+import { APP_COLORS } from '../../styles/color';
+import { style } from './style';
 
 const TaskList = ({ taskList }) => (
-	<List>
-	  {taskList.map(task => <ListItem key={task.id} title={task.content} />)}
+	<List containerStyle={style.list}>
+	  {taskList.map(task => (
+	  	<ListItem 
+		  	key={task.id} 
+		  	title={task.content}
+		  	badge={{
+		  		element: (
+		  			<Badge 
+			  			value={task.status} 
+			  			containerStyle={
+			  			task.status === TASK.todoStatus
+			  				? { backgroundColor: APP_COLORS.accent }
+			  				: { backgroundColor: APP_COLORS.lightPrimaryColor }
+						}
+			  		/>
+			  	)
+			 }}
+		/>
+	  ))}
 	</List>
 );
 
